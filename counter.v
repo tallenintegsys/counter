339a63a64cce923a0bef8576dbb196554680f61a
Tim Allen <tallen@integsys.biz>
2018-04-10 21:49:56 -0400

    pruning stuff

--- counter.v
+++ counter.v
@@ -15,50 +15,35 @@ module counter (
 reg [31:0] count;
 wire reset;
 reg [3:0] bcd;
-wire [6:0] seg;
-wire [6:0] bigseg0;
-wire [6:0] bigseg1;
-wire [6:0] bigseg2;
-wire [6:0] bigseg3;
 reg [6:0] bigseg;
+wire [6:0] seg0;
+wire [6:0] seg1;
+wire [6:0] seg2;
+wire [6:0] seg3;
 reg [1:0] sel;
 
 bcdto7seg hex0 (
 	.bcd(count[19:16]), 
-	.seg(HEX0)
+	.seg(seg0)
 );
 bcdto7seg hex1 (
 	.bcd(count[23:20]), 
-	.seg(HEX1)
+	.seg(seg1)
 );
 bcdto7seg hex2 (
 	.bcd(count[27:24]), 
-	.seg(HEX2)
+	.seg(seg2)
 );
 bcdto7seg hex3 (
 	.bcd(count[31:28]), 
-	.seg(HEX3)
-);
-bcdto7seg bighex0 (
-	.bcd(~count[19:16]),
-	.seg(bigseg0)
-);
-bcdto7seg bighex1 (
-	.bcd(~count[23:20]),
-	.seg(bigseg1)
-);
-bcdto7seg bighex2 (
-	.bcd(~count[27:24]),
-	.seg(bigseg2)
-);
-bcdto7seg bighex3 (
-	.bcd(~count[31:28]),
-	.seg(bigseg3)
+	.seg(seg3)
 );
 
 assign LEDG[1:0] = SW[1:0];
-assign HEX4[6:0] = bigseg2;
-assign HEX5[6:0] = bigseg3;
+assign HEX0[6:0] = seg0;
+assign HEX1[6:0] = seg1;
+assign HEX2[6:0] = seg2;
+assign HEX3[6:0] = seg3;
 assign reset = KEY[0];
 assign GPIO[6:0] = bigseg;
 assign GPIO[9:8] = sel;
@@ -69,18 +54,12 @@ if (~reset) begin
 	bcd[3:0] <= 4'b0000;
 end else begin
 	count <= count - 1;
-	if (count == 0) begin
-		count <= 32'hffffffff;
-		LEDR[0] <= ~LEDR[0];
-		bcd <= bcd + 1;
-		LEDR[17:14] <= bcd;
-	end
 	if (count[20]) begin
 		sel <= 2'b01;
-		bigseg <= ~bigseg3;
+		bigseg <= ~seg3;
 	end else begin
 		sel <= 2'b10;
-		bigseg <= ~bigseg2;
+		bigseg <= ~seg2;
 	end
 end
 endmodule
